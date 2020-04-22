28ca8b720c85599b4b660caafe7cc3e160644f74
r1oga <r1oga@tuta.io>
2020-04-22 11:10:31 +0200

    feat: Style Flat component with rimble ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,10 @@
 import App from 'next/app'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+import { theme } from 'rimble-ui'
 
 export default class MyApp extends App {
-  render() {
+  render () {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
