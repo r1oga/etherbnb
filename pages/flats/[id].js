21869a6b78811fb43d7094b8c383758b90b5df16
r1oga <r1oga@tuta.io>
2020-04-22 11:59:43 +0200

    feat: Add title to flat page

--- pages/flats/[id].js
+++ pages/flats/[id].js
@@ -1,8 +1,17 @@
 import * as _ from 'lodash'
+import Head from 'next/head'
 
 import flats from '../flats.json'
 import FlatComponent from '../../components/flat'
-const Flat = ({ flat }) => <FlatComponent {...flat} />
+
+const Flat = ({ flat }) => (
+  <>
+    <Head>
+      <title>{flat.title}</title>
+    </Head>
+    <FlatComponent {...flat} />
+  </>
+)
 
 Flat.getInitialProps = ({ query: { id } }) => {
   return { flat: _.filter(flats, ['id', id])[0] }
