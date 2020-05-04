873f8f01865c085add8f14716cd46405606e8098
r1oga <r1oga@tuta.io>
2020-05-04 23:11:04 +0200

    Add page title on root page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Heading, Flex, Box } from 'rimble-ui'
 import fetch from 'isomorphic-unfetch'
+import Head from 'next/head'
 
 import flats from './flats.json'
 import Flat from '../components/Flat'
@@ -7,6 +8,7 @@ import Layout from '../components/Layout'
 
 const Index = ({ flats }) => (
   <Layout>
+    <Head><title>Rbnb</title></Head>
     <Heading.h2 textAlign='center'>Places to stay at</Heading.h2>
     <Flex flexWrap='wrap' justifyContent='space-around' my={0}>
       {flats.map((flat, index) => (
