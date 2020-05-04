0522d3c30ee85bf241f06dccacf4a232c8db4a7f
r1oga <r1oga@tuta.io>
2020-05-04 21:43:10 +0200

    Fix sequelize connection options

--- db.js
+++ db.js
@@ -3,9 +3,19 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: true,
-    rejectUnauthorized: false
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 })
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err)
+  })
 
 module.exports = sequelize
