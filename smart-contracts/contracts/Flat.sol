1cfbcb4d03f269f8153fc3d588d5d4685ddd511b
r1oga <r1oga@tuta.io>
2020-06-08 20:49:24 +0200

    feat:refactor: Use SolidityUnorderedSet Library for CRUD pattern

--- smart-contracts/contracts/Flat.sol
+++ smart-contracts/contracts/Flat.sol
@@ -1,28 +1,34 @@
 pragma solidity ^0.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./SolidityUnorderedKeySet.sol";
 
 
 contract Flat is Ownable {
+    using SafeMath for uint;
+    using SolidityUnorderedKeySetLib for SolidityUnorderedKeySetLib.Set;
+
     // VARIABLES
+    SolidityUnorderedKeySetLib.Set staysSet;
     string public id;
-    enum StayStatus { undefined, booked, cancelled, checkedIn, checkedOut }
-    enum Ratings { undefined, bad, ok, good }
+    enum Status { undefined, booked, checkedIn, checkedOut }
+    enum Rating { undefined, bad, ok, good }
 
     struct Stay {
+        address guest;
         uint start;
         uint end;
-        uint index;
-        address guest;
-        Ratings rating;
-        StayStatus status;
+        Rating rating;
+        Status status;
     }
 
     mapping(bytes32 => Stay) public stays;
-    bytes32[] internal stayKeys;
 
     // EVENTS
     event StayBooked(address guest, uint start, uint end);
+    event StayUpdated(address guest, uint start, uint end, Rating rating, Status status);
+    event StayCancelled(address guest, uint start, uint end);
 
     // CONSTRUCTOR
     constructor (string memory _id, address _host) public {
@@ -42,14 +48,16 @@ contract Flat is Ownable {
 
     function bookStay(uint start, uint end) public {
         // Generate Key
-        bytes32 hash = getStayKey(msg.sender, start);
+        bytes32 key = getStayKey(msg.sender, start);
 
         // Store stay
-        stays[hash].start = start;
-        stays[hash].end = end;
-        stays[hash].index = stayKeys.length;
-        stays[hash].guest = msg.sender;
-        stays[hash].status = StayStatus.booked;
+        staysSet.insert(key); // will fail if key already exists
+        Stay storage s = stays[key];
+        s.start = start;
+        s.end = end;
+        s.guest = msg.sender;
+        s.status = Status.booked;
+        // s.rating undefined
 
         emit StayBooked(msg.sender, start, end);
     }
@@ -59,16 +67,52 @@ contract Flat is Ownable {
     view
     returns
     (
+        address guest,
+        uint start,
         uint end,
-        StayStatus status,
-        Ratings rating
+        Status status,
+        Rating rating
     )
     {
-        bytes32 hash = getStayKey(_guest, _start);
-        end = stays[hash].end;
-        rating = stays[hash].rating;
-        status = stays[hash].status;
+        bytes32 key = getStayKey(_guest, _start);
+        require(staysSet.exists(key), "No stay exists for this guest & starting date pair");
+
+        Stay storage s = stays[key];
+        guest = s.guest;
+        start = s.start;
+        end = s.end;
+        rating = s.rating;
+        status = s.status;
     }
+
+    // function cancelStay(uint _start)
+    // public
+    // {
+    //     // get Stay's key
+    //     bytes32 key = getStayKey(msg.sender, _start);
+    //
+    //     // check that caller has booked this stay
+    //     address guest = stays[key].guest;
+    //     require(guest == msg.sender, "Only the person who booked a Stay can cancel it");
+    //
+    //     // check that the start date is in more than 7 days
+    //     uint seven = 7;
+    //     require(now.add(seven.mul(1 days)) <= _start, "This Stay starts in less than 7 days: too late to cancel it");
+    //
+    //     // get index of Stay to delete
+    //     uint indexStayToDelete = stays[key].index;
+    //
+    //     // Replace stay to be deleted with last stay in the array
+    //     // Get key of stay in last position of the keys array
+    //     bytes32 fillStayHash = stayKeys[stayKeys.length.sub(1)];
+    //     // Update key at indexStayToDelete in keys array
+    //     stayKeys[indexStayToDelete] = fillStayHash;
+    //     // Set fillStay index to index of Stay to delete
+    //     stays[fillStayHash].index = indexStayToDelete;
+    //     // Reduce length of keys array
+    //     // set key to null in mapping
+    //
+    // }
 
     function _getStayKey(address _guest, uint _start)
     internal
@@ -79,9 +123,6 @@ contract Flat is Ownable {
     }
 
     // CORE FUNCTIONS
-    // function cancelStay(uint _start) public {
-    //
-    // }
     //
     // function checkIn() public {
     //
---
+++ smart-contracts/contracts/SolidityUnorderedKeySet.sol
@@ -0,0 +1,88 @@
+pragma solidity ^0.6;
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+
+library SolidityUnorderedKeySetLib {
+    using SafeMath for uint;
+
+    struct Set {
+        mapping(bytes32 => uint) keyPointers;
+        bytes32[] keyList;
+    }
+
+    function insert(Set storage self, bytes32 key) internal {
+        require(key != 0x0, "Key cannot be 0x0");
+        require(!exists(self, key), "Key already exists in the set.");
+        self.keyList.push(key);
+        self.keyPointers[key] = len(self).sub(1);
+    }
+
+    function remove(Set storage self, bytes32 key) internal {
+        require(exists(self, key), "Key does not exist in the set.");
+        bytes32 keyToMove = self.keyList[len(self).sub(1)];
+        uint rowToReplace = self.keyPointers[key];
+        self.keyPointers[keyToMove] = rowToReplace;
+        self.keyList[rowToReplace] = keyToMove;
+        delete self.keyPointers[key];
+        self.keyList.pop();
+    }
+
+    function len(Set storage self) internal view returns (uint) {
+        return self.keyList.length;
+    }
+
+    function exists(Set storage self, bytes32 key) internal view returns (bool) {
+        if (len(self) == 0) return false;
+        return self.keyList[self.keyPointers[key]] == key;
+    }
+
+    function keyAtIndex(Set storage self, uint index) internal view returns (bytes32) {
+        return self.keyList[index];
+    }
+
+    function indexAtKey(Set storage self, bytes32 key) internal view returns (uint) {
+        return self.keyPointers[key];
+    }
+
+    function nuke(Set storage self) public {
+        delete self.keyList;
+    }
+}
+
+
+contract SolidityUnorderedKeySet {
+    using SolidityUnorderedKeySetLib for SolidityUnorderedKeySetLib.Set;
+    SolidityUnorderedKeySetLib.Set set;
+
+    event LogUpdate(address sender, string action, bytes32 key);
+
+    function exists(bytes32 key) public view returns(bool) {
+        return set.exists(key);
+    }
+
+    function insert(bytes32 key) public {
+        set.insert(key);
+        emit LogUpdate(msg.sender, "insert", key);
+    }
+
+    function remove(bytes32 key) public {
+        set.remove(key);
+        emit LogUpdate(msg.sender, "remove", key);
+    }
+
+    function len() public view returns(uint) {
+        return set.len();
+    }
+
+    function keyAtIndex(uint index) public view returns(bytes32) {
+        return set.keyAtIndex(index);
+    }
+
+    function indexAtKey(bytes32 key) public view returns (uint) {
+        return set.indexAtKey(key);
+    }
+
+    function nuke() public {
+        set.nuke();
+    }
+}
